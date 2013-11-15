c509902713a13a2fe3e051219be4b940afe37e57
benkibejs <benlind@student.chalmers.se>
2013-11-15 15:58:10 +0100

    Ändring 1

---
+++ lab2/src/geometri/Circle.java
@@ -0,0 +1,74 @@
+package geometri;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Circle implements GeometricalForm {
+
+	@Override
+	public int compareTo(GeometricalForm arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void fill(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPerimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void move(int dx, int dy) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void place(int x, int y) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ lab2/src/geometri/GeometricalFormAbstract.java
@@ -0,0 +1,37 @@
+package geometri;
+
+import java.awt.*;
+
+public abstract class GeometricalFormAbstract implements GeometricalForm{
+	protected int posX;
+	protected int posY;
+	protected Color color;
+	
+	@Override
+	public Color getColor() {
+		return color;
+	}
+	
+	@Override
+	public void place(int x, int y) {
+	posX=x;
+	posY=y;
+	}
+	
+	@Override
+	public void move(int dx, int dy) {
+	posX = posX + dx;
+	posY = posY + dy;
+	}
+	
+	@Override
+	public int getX() {
+		return posX;
+	}
+	
+	@Override
+	public int getY() {
+		return posY;
+	}
+	
+}
